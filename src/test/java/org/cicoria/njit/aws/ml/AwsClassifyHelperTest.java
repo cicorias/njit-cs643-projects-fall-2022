47d9a9ccb952d1cf46a7198f0a29e087c10e9f2e
Shawn Cicoria <github@cicoria.com>
2022-09-20 13:54:07 -0400

    updated aws sdk version
    added rekognition work
    fixed some logic
    refactor for v2 of
    aws sdk

---
+++ src/test/java/org/cicoria/njit/aws/ml/AwsClassifyHelperTest.java
@@ -0,0 +1,23 @@
+package org.cicoria.njit.aws.ml;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AwsClassifyHelperTest {
+
+    @Test
+    void getLabelsForS3Image() {
+        var helper = new AwsClassifyHelper();
+
+        String bucket = "test-bucket-njit";
+        String key = "1.jpg";
+        String expected = "Car";
+        int expectedValueMin = 90;
+        var rv = helper.GetLabelsForS3Image(bucket, key);
+
+        var actual = rv.get("car");
+        assertTrue(actual > expectedValueMin);
+
+    }
+}
